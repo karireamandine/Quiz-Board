de3a908e8d4f4b8e7a5e62950f424e060204d831
karire amandine <karireamandine@gmail.com>
2018-12-21 11:26:48 +0200

    add function check

---
+++ script.js
@@ -0,0 +1,20 @@
+function check() {
+    var questionOne = document.Quiz.questionOne.value;
+    var questionTwo = document.Quiz.questionTwo.value;
+    var questionThree = document.Quiz.questionThree.value;
+    var questionFour = document.Quiz.questionFour.value;
+    var questionFive = document.Quiz.questionFive.value;
+    var right = 0;
+    if (questionOne == "correct") {
+        right = right + 5;}
+    if (questionThree == "correct") {
+        right = right + 5;
+    
+    
+
+    };
+    document.getElementById("fifth-p").style.visibility = "visible";
+    document.getElementById("correctNumber").innerHTML = "HERE IS YOUR SCORE: " + right + "/50";
+    $(".container").fadeOut();
+
+};
